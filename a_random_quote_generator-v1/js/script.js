4c0a27882689237e3c8b2f9ee01295bd33872723
Christopher Fritz Wester <103167502+Mordul212@users.noreply.github.com>
2026-03-30 22:27:05 -0500

    Update with color
    random color

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -12,35 +12,59 @@ project 1 - A Random Quote Generator
 ***/
 
 quotes = [
-  ["May the Force be with you", "Yoda", "Star Wars", "1977" ],
-  ["There's no place like home", "Dorothy", "The Wizard of Oz", "1939" ],
-  ["You can't handle the truth!", "Col. Nathan R. Jessep", "A Few Good Men", "1992" ],
-  ["You're gonna need a bigger boat", "Chief Martin Brody", "Jaws", "1975" ],
-  ["I'll be back", "T-800", "The Terminator", "1984" ],
+  { quote : "May the Force be with you", source : "Yoda", citation : "Star Wars", year : "1977", genre : "Science Fiction" },
+  { quote : "There's no place like home", source : "Dorothy", citation : "The Wizard of Oz", year : "1939", genre : "Fantasy" },
+  { quote : "You can't handle the truth!", source : "Col. Nathan R. Jessep", citation : "A Few Good Men", year : "1992", genre : "Drama" },
+  { quote : "You're gonna need a bigger boat", source : "Chief Martin Brody", citation : "Jaws", year : "1975", genre : "Thriller" },
+  { quote : "I'll be back", source : "T-800", citation : "The Terminator", year : "1984", genre : "Science Fiction" },
+  //{ quote : "No Citation", source : "No Year", citation : "", year : "" } // Test quote
 ];
+
+// function to change background color
+function changeBGColor() {
+  let redColor = Math.floor( Math.random() * 255 );
+  let greenColor = Math.floor( Math.random() * 255 );
+  let blueColor = Math.floor( Math.random() * 255 );
+  // Source - https://stackoverflow.com/a/197761
+  // Posted by user7094, modified by community. See post 'Timeline' for change history
+  // Retrieved 2026-03-30, License - CC BY-SA 4.0
+  document.body.style.backgroundColor = `rgb(${redColor}, ${greenColor}, ${blueColor})`; //Divakar Rajesh helped with rgb syntax
+}
 
 /***
  * `getRandomQuote` function
 ***/
-let quote = [];
-let lastQuote = ["Every great developer you know got there by solving problems they were unqualified to solve until they actually did it."];
+let choice = [];
+let lastQuote = [];
 
 function getRandomQuote() {
   do {
-  quote = quotes[ Math.floor( Math.random() * quotes.length ) ]; //randonly grab quote from selection
-  } while ( quote[0] === lastQuote[0] ); // compare grabbed quote with previous displayed quote
-  lastQuote = quote;
-  return quote;
+  let chance = Math.floor( Math.random() * quotes.length ) // NECESSARY - variable to store random number ranging from zero to the index of the last item in the quotes array.
+  choice = quotes[ chance ]; //randomly grab quote from selection
+  } while ( choice.quote === lastQuote.quote ); // compare grabbed quote with previous displayed quote
+  lastQuote = choice; // update lastquote to new choice
+  return choice;
 }
-
-console.log(getRandomQuote());
 
 /***
  * `printQuote` function
 ***/
 
 function printQuote() {
-  document.getElementById('quote-box').innerHTML = getRandomQuote();
+  let grab = getRandomQuote();// NECESSARY - variable to store a random quote object returned from the getRandomQuote() function.
+  let quoteHTML = `<p class = "quote"> ${grab.quote}</p> <p class = "source"> ${grab.source}`;
+  if (choice.citation !== ""){
+      quoteHTML += `<span class="citation"> ${grab.citation} </span>`
+      }
+  if (choice.year !== ""){
+      quoteHTML += `<span class="year"> ${grab.year} </span>`
+      } 
+  if (choice.genre !== ""){ // additional property
+      quoteHTML += `<span class="genre"> ${grab.genre} </span>`
+      } 
+  quoteHTML += "</p>" // close paragraph element
+  document.getElementById('quote-box').innerHTML = quoteHTML; // change displayed quote on webpage
+  changeBGColor();
 }
 
 /***
